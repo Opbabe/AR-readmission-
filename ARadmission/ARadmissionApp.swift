b3bc793d3dc668cb50daba885bf73ab560e30950
Opbabe <nicktran900@gmail.com>
2025-10-13 22:46:59 -0700

    Initial Commit

---
+++ ARadmission/ARadmissionApp.swift
@@ -0,0 +1,33 @@
+//
+//  ARadmissionApp.swift
+//  ARadmission
+//
+//  Created by Nick Tran on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ARadmissionApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ ARadmissionTests/ARadmissionTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARadmissionTests.swift
+//  ARadmissionTests
+//
+//  Created by Nick Tran on 10/13/25.
+//
+
+import Testing
+@testable import ARadmission
+
+struct ARadmissionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
